983970f55104328051048bd01180a1ba3d221aaf
anas <anasnasim1@gmail.com>
2025-05-05 17:54:26 +0530

    Revamp

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__, 
     static_url_path='/static',
     static_folder='static')
 
+@app.route('/static/site.webmanifest')
+def manifest():
+    return send_from_directory('static', 'site.webmanifest', mimetype='application/manifest+json')
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
